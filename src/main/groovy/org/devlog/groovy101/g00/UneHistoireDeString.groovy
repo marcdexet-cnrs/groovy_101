6e7effe8f2bd66ca40b1c8c7b2e85c08bf696e6f
marcdexet <marcdexet@vm-0.marcdexet.koding.kd.io>
2014-03-07 22:52:00 +0000

    détails

--- src/main/groovy/org/devlog/groovy101/g00/UneHistoireDeString.groovy
+++ src/main/groovy/org/devlog/groovy101/g00/UneHistoireDeString.groovy
@@ -8,6 +8,7 @@
 /*
  * Chaine simple quote sans interpolation
  */
+
 def str0 = 'une chaine simple et directe'
 def myName = 'Bond, James Bond'
 
@@ -16,6 +17,8 @@ def myName = 'Bond, James Bond'
 */
 def str1 ="My name is $myName"
 println str1
+
+
 
 /*
  * chaine sur plusieurs lignes : 3 quotes
@@ -26,3 +29,16 @@ seconde ligne
 dernière ligne"""
 
 println str2
+
+/*
+ * Il est possible de le faire directement avec un print ou un println
+ */
+ 
+println """
+-----------------------------------------------------------
+
+  My name is $str1
+
+-----------------------------------------------------------
+
+"""
