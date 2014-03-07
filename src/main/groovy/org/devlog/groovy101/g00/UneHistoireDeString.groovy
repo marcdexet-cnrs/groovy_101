9ee4841384c4aae2facf3e5e3597faf074999ff1
marcdexet <marcdexet@vm-0.marcdexet.koding.kd.io>
2014-03-07 20:37:32 +0000

    Histoire de string

---
+++ src/main/groovy/org/devlog/groovy101/g00/UneHistoireDeString.groovy
@@ -0,0 +1,28 @@
+/*
+* ============================================
+* Les chaines en groovy
+* ============================================
+*/
+
+
+/*
+ * Chaine simple quote sans interpolation
+ */
+def str0 = 'une chaine simple et directe'
+def myName = 'Bond, James Bond'
+
+/*
+* Chaine interpolée avec double quote
+*/
+def str1 ="My name is $myName"
+println str1
+
+/*
+ * chaine sur plusieurs lignes : 3 quotes
+ */ 
+def str2 = """Voici une chaine sur plusieurs lignes
+première ligne
+seconde ligne
+dernière ligne"""
+
+println str2
